8855de7b4858acfb8c276749c973ecffa0ea5215
Anton <antklim89@mail.ru>
2022-04-03 21:46:25 +0300

    created theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,24 @@
 import '~/styles/globals.scss';
+import { ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
 
 import Layout from '~/layouts/Layout';
+
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/400-italic.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/roboto/700-italic.css';
 
+import { theme } from '../styles/theme';
+
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <ThemeProvider theme={theme}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ThemeProvider>
     );
 };
 
