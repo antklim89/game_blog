cdbe94c40cd5ab9545bfb6266df713bb8db5eccd
Anton <antklim89@mail.ru>
2022-04-04 00:54:42 +0300

    updated Header component

---
+++ layouts/Header/HeaderLogo.tsx
@@ -0,0 +1,16 @@
+import Typography from '@mui/material/Typography';
+import Link from 'next/link';
+import { FC } from 'react';
+
+
+const HeaderLogo: FC = () => {
+    return (
+        <Link passHref href="/" key="2">
+            <Typography component="a" fontWeight="bold" variant="h5">
+                GAME BLOG
+            </Typography>
+        </Link>
+    );
+};
+
+export default HeaderLogo;
