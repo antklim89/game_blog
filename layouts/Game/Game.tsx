88f412f8ca1506e462e2da4a216d49fd7cf8bf04
Anton <antklim89@mail.ru>
2022-04-20 00:04:26 +0300

    updated Game component

--- layouts/Game/Game.tsx
+++ layouts/Game/Game.tsx
@@ -1,13 +1,24 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import { FC } from 'react';
 
 import { IGame } from '~/types';
 
 
-const Game: FC<IGame> = ({ body }) => {
+const Game: FC<IGame> = ({ body, title, genre, year, publisher }) => {
     return (
-        <div>
-            {body}
-        </div>
+        <article>
+            <Typography variant="h1">
+                {title}
+            </Typography>
+            <Typography component="h2" variant="h2">
+                {publisher}
+            </Typography>
+            <Typography component="h5" sx={{ mb: 4 }} variant="h5">
+                {genre} - {year} year
+            </Typography>
+            <Box dangerouslySetInnerHTML={{ __html: body }} />
+        </article>
     );
 };
 
