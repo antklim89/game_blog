960f4b6f7b8e69e87e9887b6728a6886f468bb2e
Anton <antklim89@mail.ru>
2022-04-25 22:04:59 +0300

    updated NewsListItem component

--- layouts/NewsList/NewsListItem.tsx
+++ layouts/NewsList/NewsListItem.tsx
@@ -1,17 +1,41 @@
+import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
+import Image from 'next/image';
+import Link from 'next/link';
 import { FC } from 'react';
 
 import { NewsListItemProps } from './NewsList.types';
 
 
-const NewsListItem: FC<NewsListItemProps> = ({ title }) => {
+const NewsListItem: FC<NewsListItemProps> = ({ title, publishedAt, slug, body, previewImage }) => {
+
     return (
-        <ListItem key={title}>
-            <ListItemText>
-                {title}
-            </ListItemText>
-        </ListItem>
+        <>
+
+            <Link passHref href={`/news/${slug}`}>
+                <ListItem component="a" sx={{ alignItems: 'flex-start', flexDirection: 'column' }}>
+                    {/* <ListItemAvatar>
+                        <Image
+                            alt="title"
+                            height={3400}
+                            src="http://192.168.90.19:3000/content/news/images/exiter23.jpg"
+                            width={3400}
+                        />
+                    </ListItemAvatar> */}
+                    <ListItemText
+                        primary={title}
+                        primaryTypographyProps={{ variant: 'h4', component: 'h4' }}
+                        secondary={publishedAt}
+                    />
+                    <ListItemText
+                        primary={body.slice(0, 200)}
+                    />
+                </ListItem>
+            </Link>
+            <Divider component="li" variant="inset" />
+        </>
     );
 };
 
