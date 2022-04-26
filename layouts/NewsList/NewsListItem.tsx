91b187ccb7d04d23f0af0644fab413d8cbd3fec1
Anton <antklim89@mail.ru>
2022-04-26 19:45:48 +0300

    updated NewsListItem component

--- layouts/NewsList/NewsListItem.tsx
+++ layouts/NewsList/NewsListItem.tsx
@@ -1,3 +1,4 @@
+import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
@@ -10,28 +11,29 @@ import { NewsListItemProps } from './NewsList.types';
 
 
 const NewsListItem: FC<NewsListItemProps> = ({ title, publishedAt, slug, body, previewImage }) => {
-
     return (
         <>
 
             <Link passHref href={`/news/${slug}`}>
-                <ListItem component="a" sx={{ alignItems: 'flex-start', flexDirection: 'column' }}>
-                    {/* <ListItemAvatar>
+                <ListItem component="a">
+                    <ListItemAvatar sx={{ display: ['none', 'block'], mr: 4 }}>
                         <Image
                             alt="title"
-                            height={3400}
-                            src="http://192.168.90.19:3000/content/news/images/exiter23.jpg"
-                            width={3400}
+                            height={128}
+                            src={`http://localhost:3000${previewImage}`}
+                            width={128}
                         />
-                    </ListItemAvatar> */}
-                    <ListItemText
-                        primary={title}
-                        primaryTypographyProps={{ variant: 'h4', component: 'h4' }}
-                        secondary={publishedAt}
-                    />
-                    <ListItemText
-                        primary={body.slice(0, 200)}
-                    />
+                    </ListItemAvatar>
+                    <Box>
+                        <ListItemText
+                            primary={title}
+                            primaryTypographyProps={{ variant: 'h4', component: 'h4' }}
+                            secondary={publishedAt}
+                        />
+                        <ListItemText
+                            primary={body.slice(0, 200)}
+                        />
+                    </Box>
                 </ListItem>
             </Link>
             <Divider component="li" variant="inset" />
