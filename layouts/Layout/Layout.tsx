642212b5bccab1c9144802ec40026e5b0018c45d
Anton <antklim89@mail.ru>
2022-04-26 20:39:58 +0300

    updated packages

--- layouts/Layout/Layout.tsx
+++ layouts/Layout/Layout.tsx
@@ -1,10 +1,10 @@
-import { FC } from 'react';
+import { FC, PropsWithChildren } from 'react';
 
 import Footer from '../Footer';
 import Header from '../Header';
 
 
-const Layout: FC = ({ children }) => {
+const Layout: FC<PropsWithChildren<unknown>> = ({ children }) => {
     return (
         <div className="root">
             <Header />
