1a43b6e8c60c9846a2085a1009532eeb3994346a
Anton <antklim89@mail.ru>
2022-04-27 05:59:44 +0300

    updated styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,28 @@
 import '~/styles/globals.scss';
-import { ThemeProvider } from '@mui/material';
-import type { AppProps } from 'next/app';
 
+import { Theme, ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import type { AppProps } from 'next/app';
+import { useCallback } from 'react';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/400-italic.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/roboto/700-italic.css';
 
-import { theme } from '../styles/theme';
+import { theme } from '~/styles/theme';
 
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+    const styles = useCallback(({ palette }: Theme) => ({
+        b: { color: palette.secondary.main },
+        strong: { color: palette.secondary.main },
+    }), []);
+
     return (
         <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <GlobalStyles styles={styles} />
             <Component {...pageProps} />
         </ThemeProvider>
     );
