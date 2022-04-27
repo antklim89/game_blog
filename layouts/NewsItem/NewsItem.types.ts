05b0b37d77cf0bae0881ce46c008e5fbb7f448a8
Anton <antklim89@mail.ru>
2022-04-27 05:59:44 +0300

    updated styles

---
+++ layouts/NewsItem/NewsItem.types.ts
@@ -0,0 +1,5 @@
+
+
+export interface NewsItemProps {
+     // null
+}
--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,10 +1,22 @@
 import { createTheme } from '@mui/material';
-import { purple, amber } from '@mui/material/colors';
+import { deepPurple, amber, blueGrey } from '@mui/material/colors';
+
+
+const { 900: primaryColor } = deepPurple;
+const { 50: background } = blueGrey;
+const { 900: secondary } = amber;
 
 
 export const theme = createTheme({
     palette: {
-        primary: { main: purple[900] },
-        secondary: { main: amber[900] },
+        primary: { main: primaryColor },
+        secondary: { main: secondary },
+        background: { default: background },
+    },
+    typography: {
+        h1: { color: primaryColor },
+        h2: { color: primaryColor },
+        h3: { color: primaryColor },
+        h4: { color: primaryColor },
     },
 });
