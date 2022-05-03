4b8127bb5ad7dcf4b591cfd1a3003f51d52d58c0
Anton <antklim89@mail.ru>
2022-05-03 14:17:39 +0300

    updated contacts

---
+++ pages/contacts/success.tsx
@@ -0,0 +1,40 @@
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import type { NextPage } from 'next';
+import Link from 'next/link';
+
+import Seo from '~/components/Seo';
+import Layout from '~/layouts/Layout';
+
+
+const ContactsPage: NextPage = () => {
+    return (
+        <Layout>
+            <Seo title="Contacts" />
+            <Container>
+                <Paper sx={{ p: 4 }}>
+                    <Typography
+                        component="p"
+                        fontSize={['2rem', '4rem']}
+                        mb={4}
+                        py={20}
+                        textAlign="center"
+                        textTransform="uppercase"
+                        variant="h3"
+                    >
+                        the message was sent successfully
+                    </Typography>
+                    <Link passHref href="/">
+                        <Button component="a" variant="contained">
+                            &larr;&emsp;go home
+                        </Button>
+                    </Link>
+                </Paper>
+            </Container>
+        </Layout>
+    );
+};
+
+export default ContactsPage;
