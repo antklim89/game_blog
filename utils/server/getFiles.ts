cf4c5b8232d4c662364af63a5b60323c5fd0e57e
Anton <antklim89@mail.ru>
2022-05-11 00:47:58 +0300

    added memoize

--- utils/server/getFiles.ts
+++ utils/server/getFiles.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 
-import { readJSON, readdir } from 'fs-extra';
+import { readJSON, readdir, statSync } from 'fs-extra';
+import memoize from 'lodash/memoize';
 
 import { GetFilesResult } from '~/types';
 
@@ -12,28 +13,35 @@ export interface GetFilesOptions {
     page?: number;
 }
 
+
+const getMemoizedFiles = memoize(async (fileDir: string) => {
+    const fileNames = await readdir(fileDir);
+
+    return Promise.all(fileNames
+        .filter((filename) => (/\.json/i).test(filename))
+        .map((fileName) => ({
+            fileName,
+            time: statSync(resolve(fileDir, fileName)).mtime.getTime(),
+        }))
+        .sort((a, b) => b.time - a.time)
+        .map(({ fileName }) => fileName)
+        .map(async (fileName) => {
+            const fileJson = await readJSON(resolve(fileDir, fileName), 'utf-8');
+            fileJson.slug = fileName.replace(/\.json/i, '');
+            return fileJson;
+        }));
+});
+
+
 export async function getFiles<T>(
     filePath: string,
     { limit = 6, page = 1 }: GetFilesOptions = { limit: 6, page: 1 },
 ): Promise<GetFilesResult<T>> {
-    const files = await readdir(resolve(process.cwd(), CONTENT_PATH, filePath));
-    const totalPages = Math.ceil(files.length / limit);
-
-    const jsonFiles = files
-        .reverse()
-        .filter((item) => (/\.json/i).test(item))
-        .slice((page - 1) * limit, (page * limit));
-
-    const items = await Promise.all(jsonFiles.map(async (fileName) => {
-        const fileJson = await readJSON(resolve(process.cwd(), CONTENT_PATH, filePath, fileName), 'utf-8');
-        fileJson.slug = fileName.replace(/\.json/i, '');
-        return fileJson;
-    }));
+    const fileDir = resolve(process.cwd(), CONTENT_PATH, filePath);
+    const fileJsons = await getMemoizedFiles(fileDir);
 
     return {
-        items,
-        totalPages,
+        items: fileJsons.slice((page - 1) * limit, (page * limit)) as T[],
+        totalPages: Math.ceil(fileJsons.length / limit),
     };
 }
-
-
