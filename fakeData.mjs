b428209e4a622eec5ba5123fe567971ab14687f1
Anton <antklim89@mail.ru>
2022-05-11 01:18:24 +0300

    updated fake news

--- fakeData.mjs
+++ fakeData.mjs
@@ -1,40 +1,28 @@
 import fs from 'fs';
 import path from 'path';
 
+import faker from '@faker-js/faker';
 import _ from 'lodash';
 
+
+const NEWS_NUMBER = 24;
 
 /**
  * News
  */
 const newsDir = path.resolve('public/content/news');
-_.times(1, (index) => {
-    const title = `Test ${index}`;
+for (let index = 0; index < NEWS_NUMBER; index += 1) {
+    const title = faker.lorem.words(_.random(3, 6, false));
     const dateNow = new Date();
-    const fileName = getFilename(title, dateNow);
+    dateNow.setMinutes(index);
+    const fileName = `${_.kebabCase(title)}.json`;
 
     const newsItem = JSON.stringify({
         publishedAt: dateNow.toUTCString(),
-        body: 'Lorem ipsum',
+        body: faker.lorem.paragraphs(_.random(3, 6, false)),
         title,
         previewImage: '/wesley-tingey--l4UCK06uNM-unsplash.jpg',
     }, null, 4);
 
     fs.writeFileSync(path.resolve(newsDir, fileName), newsItem);
-});
-
-
-/**
- * Utils
- */
-function getFilename(title, dateNow) {
-    const y = dateNow.getFullYear();
-    const m = dateNow.getMonth();
-    const d = dateNow.getDay();
-    const h = dateNow.getHours();
-    const min = dateNow.getMinutes();
-    const s = dateNow.getSeconds();
-    const fileName = `${_.kebabCase(`${y}${m}${d}${h}${min}${s}-${title}`)}.json`;
-
-    return fileName;
 }
