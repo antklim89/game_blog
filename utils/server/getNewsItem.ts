a4de005197845a8850c98502ca67d59556b0bfe7
Anton <antklim89@mail.ru>
2022-06-18 19:44:24 +0300

    updated getNewsItem

--- utils/server/getNewsItem.ts
+++ utils/server/getNewsItem.ts
@@ -5,7 +5,6 @@ import { getFile } from './getFile';
 
 export async function getNewsItem(slug: string) {
     const newsItem = await getFile<INews>('news', slug);
-    newsItem.publishedAt = new Date(newsItem.publishedAt).toLocaleString();
 
     return newsItem;
 }
