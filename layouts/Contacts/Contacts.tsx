631123cea4cef9884dcf4e188a92870f0c734550
Anton <antklim89@mail.ru>
2022-08-01 19:42:51 +0300

    updated contacts

--- layouts/Contacts/Contacts.tsx
+++ layouts/Contacts/Contacts.tsx
@@ -1,19 +1,37 @@
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import { FC } from 'react';
+import { FC, FormEventHandler, useState } from 'react';
 
 
 const Contacts: FC = () => {
+    const [loading, setLoading] = useState(false);
+    const [status, setStatus] = useState<'success' | 'error' | null>(null);
+
+    const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        const body = new FormData(e.target as HTMLFormElement);
+        const response = await fetch('/', { method: 'POST', body });
+
+        if (response.ok) setStatus('success');
+        else setStatus('error');
+
+        setLoading(false);
+        (e.target as HTMLFormElement).reset();
+    };
+
     return (
         <section>
             <Paper
-                action="/contacts/success"
                 component="form"
                 data-netlify="true"
-                method="post"
                 name="contact"
                 netlify-honeypot="bot-field"
                 sx={{
@@ -21,16 +39,35 @@ const Contacts: FC = () => {
                     flexDirection: 'column',
                     p: 4,
                 }}
+                onSubmit={handleSubmit}
             >
+                <input name="form-name" type="hidden" value="contact" />
+
                 <Typography component="h1" sx={{ mb: 4 }} variant="h3">
-                    Have a question?<br />Write me.
+                    Leave a message
                 </Typography>
+
                 <Box component="label" sx={{ display: 'none' }}>
                     Don&apos;t fill this out if you&apos;re human: <input name="bot-field" />
                 </Box>
+
+                {status === 'success' && (
+                    <Alert severity="success" sx={{ mb: '2rem' }}>
+                        <AlertTitle>Success</AlertTitle>
+                        Messege sent.
+                    </Alert>
+                )}
+                {status === 'error' && (
+                    <Alert severity="error" sx={{ mb: '2rem' }}>
+                        <AlertTitle>Error</AlertTitle>
+                        Unexpected error. Try again later.
+                    </Alert>
+                )}
+
                 <TextField
                     fullWidth
                     required
+                    disabled={loading}
                     label="Name"
                     name="name"
                     sx={{ mb: 4 }}
@@ -39,6 +76,7 @@ const Contacts: FC = () => {
                 <TextField
                     fullWidth
                     required
+                    disabled={loading}
                     label="Subject"
                     name="subject"
                     sx={{ mb: 4 }}
@@ -48,23 +86,22 @@ const Contacts: FC = () => {
                     fullWidth
                     multiline
                     required
+                    disabled={loading}
                     label="Message"
                     name="message"
                     rows={5}
                     sx={{ mb: 4 }}
                 />
-                <TextField
-                    fullWidth
-                    name="form-name"
-                    sx={{ display: 'none' }}
-                    value="contact"
-                />
+
                 <Button
                     color="primary"
+                    disabled={loading}
+                    sx={{ minHeight: 54, position: 'relative' }}
                     type="submit"
                     variant="contained"
                 >
                     Send
+                    {loading ? <CircularProgress color="secondary" sx={{ position: 'absolute' }} /> : null}
                 </Button>
             </Paper>
         </section>
