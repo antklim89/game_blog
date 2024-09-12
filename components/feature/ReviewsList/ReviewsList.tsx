d770997e8f797fb42a3323067e86043c1f66f11b
Anton <=>
2024-09-13 01:40:47 +0300

    fix: move reviews to props

--- components/feature/ReviewsList/ReviewsList.tsx
+++ components/feature/ReviewsList/ReviewsList.tsx
@@ -1,15 +1,13 @@
 import Grid from '@mui/material/Grid2';
 import type { FC } from 'react';
-import { getReviews } from '~/lib/contentLoaders';
+import type { IReview } from '~/lib/types';
 import ReviewListItem from './ReviewsListItem';
 
 
-const ReviewsList: FC = async () => {
-  const reviews = await getReviews({ limit: 4 });
-
+const ReviewsList: FC<{ reviews: IReview[] }> = async ({ reviews }) => {
   return (
     <Grid container spacing={1} sx={{ my: 4 }}>
-      {reviews.items.map(review => (
+      {reviews.map(review => (
         <ReviewListItem {...review} key={review.slug} />
       ))}
     </Grid>
