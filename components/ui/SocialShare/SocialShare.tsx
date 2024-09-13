c5e3bbf18a27e4f82efe13e6452b6ef027c66c81
Anton <=>
2024-09-13 05:21:34 +0300

    fix(components): update SocialShare

--- components/ui/SocialShare/SocialShare.tsx
+++ components/ui/SocialShare/SocialShare.tsx
@@ -1,5 +1,6 @@
+'use client';
 import Box from '@mui/material/Box';
-import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
 import {
   EmailIcon,
   EmailShareButton,
@@ -11,39 +12,37 @@ import {
   VKShareButton,
 } from 'react-share';
 import type { FC } from 'react';
-
 import { SITE_URL } from '~/lib/constants';
-
 import type { SocialShareProps } from './SocialShare.types';
 
 
 const SocialShare: FC<SocialShareProps> = ({ title, image }) => {
-  const { asPath } = useRouter();
+  const pathname = usePathname();
 
   return (
     <Box>
       <VKShareButton
         image={`${image}`}
         title={title}
-        url={`${SITE_URL}${asPath}`}
+        url={`${SITE_URL}${pathname}`}
       >
         <VKIcon />
       </VKShareButton>
       <EmailShareButton
         subject={title}
-        url={`${SITE_URL}${asPath}`}
+        url={`${SITE_URL}${pathname}`}
       >
         <EmailIcon />
       </EmailShareButton>
       <FacebookShareButton
         title={title}
-        url={`${SITE_URL}${asPath}`}
+        url={`${SITE_URL}${pathname}`}
       >
         <FacebookIcon />
       </FacebookShareButton>
       <RedditShareButton
         title={title}
-        url={`${SITE_URL}${asPath}`}
+        url={`${SITE_URL}${pathname}`}
       >
         <RedditIcon />
       </RedditShareButton>
