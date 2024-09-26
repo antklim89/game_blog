8bb20cdf097b62421d67e0163d1f931c8a6ef1bd
Anton <=>
2024-09-26 15:59:45 +0300

    feat(app): add generateStaticParams

--- app/review-list/[publisher]/[developer]/[genre]/[page]/page.tsx
+++ app/review-list/[publisher]/[developer]/[genre]/[page]/page.tsx
@@ -1,26 +1,73 @@
-import Container from '@mui/material/Container';
-import { notFound } from 'next/navigation';
 import type { Metadata } from 'next';
-import type { z } from 'zod';
+import Container from '@mui/material/Container';
 import ReviewList from '~/components/feature/ReviewList';
 import SelectReviewFields from '~/components/feature/SelectReviewFields';
 import Layout from '~/components/layout/Layout';
 import Pagination from '~/components/ui/Pagination';
-import { getReviews, getTopHeader } from '~/lib/contentLoaders';
-import { reviewParamsSchema } from '~/lib/schemas';
+import { getReviews, getReviewsFields, getTopHeader } from '~/lib/contentLoaders';
+import { filterContent } from '~/lib/fileLoaders';
 
+
+interface ReviewsPageProps {
+  params: {
+    genre: string;
+    publisher: string;
+    developer: string;
+    page: number;
+  };
+}
 
 export const metadata: Metadata = {
   title: 'Reviews',
 };
 
-async function ReviewsPage({ params }: { params: z.infer<typeof reviewParamsSchema> }) {
-  const {
-    publisher,
-    developer,
-    genre,
-    page,
-  } = await reviewParamsSchema.parseAsync(params).catch(() => notFound());
+export async function generateStaticParams() {
+  const reviews = await getReviews({ limit: Infinity });
+  const reviewsFields = await getReviewsFields();
+  const publishers = reviewsFields.publisher;
+  const developers = reviewsFields.developer;
+  const genres = reviewsFields.genre;
+
+  const allOptions = [
+    { publisher: 'all', developer: 'all', genre: 'all' },
+    ...publishers.map(publisher => ({ publisher, developer: 'all', genre: 'all' })),
+    ...developers.map(developer => ({ publisher: 'all', developer, genre: 'all' })),
+    ...genres.map(genre => ({ publisher: 'all', developer: 'all', genre })),
+    ...publishers.flatMap(publisher =>
+      developers.map(developer => ({ publisher, developer, genre: 'all' })),
+    ),
+    ...publishers.flatMap(publisher =>
+      genres.map(genre => ({ publisher, developer: 'all', genre })),
+    ),
+    ...developers.flatMap(developer =>
+      genres.map(genre => ({ publisher: 'all', developer, genre })),
+    ),
+  ];
+
+  const paths = allOptions.flatMap((o) => {
+    const contentFiltered = filterContent(reviews.items, {
+      publisher: o.publisher === 'all' ? undefined : o.publisher,
+      developer: o.developer === 'all' ? undefined : o.developer,
+      genre: o.genre === 'all' ? undefined : o.genre,
+    });
+
+    return Array.from({ length: Math.ceil(contentFiltered.length / 8) }, (_, i) => ({
+      page: String(i + 1),
+      publisher: encodeURIComponent(o.publisher),
+      developer: encodeURIComponent(o.developer),
+      genre: encodeURIComponent(o.genre),
+    }));
+  });
+
+  return paths;
+}
+
+async function ReviewsPage({ params }: ReviewsPageProps) {
+  const publisher = decodeURIComponent(params.publisher);
+  const developer = decodeURIComponent(params.developer);
+  const genre = decodeURIComponent(params.genre);
+  const page = params.page;
+
 
   const { reviewsImage } = await getTopHeader();
   const reviews = await getReviews({
