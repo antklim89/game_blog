e603db32e542a21ec65d284fb75b4377077a427c
Anton <=>
2024-10-01 17:16:39 +0300

    feat: add title to Review

--- components/feature/Review/Review.tsx
+++ components/feature/Review/Review.tsx
@@ -1,6 +1,6 @@
+import type { FC } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import type { FC } from 'react';
 import Markdown from '~/components/ui/Markdown';
 import SocialShare from '~/components/ui/SocialShare';
 import type { IReview } from '~/lib/types';
@@ -22,28 +22,33 @@ const Review: FC<{ review: IReview }> = ({ review }) => {
     <article>
       <Box
         display="flex"
-        flexDirection={{ xs: 'column', sm: 'column', md: 'row-reverse' }}
+        flexDirection="column"
         justifyContent="space-between"
       >
-        <SocialShare image={previewImage} title={title} />
-        <Typography sx={{ mb: 4 }}>
-          <b>Genre:</b>
-          {' '}
-          {genre}
-          <br />
-          <b>Publisher:</b>
-          {' '}
-          {publisher}
-          <br />
-          <b>Developer:</b>
-          {' '}
-          {developer}
-          <br />
-          <b>Release:</b>
-          {' '}
-          {formatDate(gameRelease)}
-          <br />
+        <Typography color="primary" component="h1" variant="h2">
+          {title}
         </Typography>
+        <Box display="flex" flexDirection={['column', 'row-reverse']} justifyContent="space-between">
+          <SocialShare image={previewImage} title={title} />
+          <Typography sx={{ mb: 4 }}>
+            <b>Genre:</b>
+            {' '}
+            {genre}
+            <br />
+            <b>Publisher:</b>
+            {' '}
+            {publisher}
+            <br />
+            <b>Developer:</b>
+            {' '}
+            {developer}
+            <br />
+            <b>Release:</b>
+            {' '}
+            {formatDate(gameRelease)}
+            <br />
+          </Typography>
+        </Box>
       </Box>
       <Markdown>
         {body}
