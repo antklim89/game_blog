959e12e5c36f0b2fe1edc096658560aca07ce672
Anton <=>
2024-10-01 13:23:04 +0300

    refactor: update cms

--- cms/index.ts
+++ cms/index.ts
@@ -1,26 +1,46 @@
-import type { InitOptions } from 'decap-cms-core';
-
+import type { CmsBackend, InitOptions } from 'decap-cms-core';
+import { REPOSITORY_URL, SITE_URL } from '~/lib/constants';
 import { about } from './about';
+import { baseMediaFolder, basePublicFolder } from './constants';
 import { gamesCarousel } from './gamesCarousel';
 import { news } from './news';
 import { reviews } from './reviews';
 import { topHeader } from './topHeader';
 
 
+const URL_HOSTNAME = new URL(process.env.URL ?? 'http://localhost:3000').hostname;
+
+const backend: CmsBackend
+  = process.env.NETLIFY === 'true'
+    ? {
+        name: 'git-gateway',
+        branch: 'main',
+      }
+    : {
+        name: 'github',
+        repo: REPOSITORY_URL,
+        branch: 'main',
+      };
+
+
 export const cmsConfig: InitOptions = {
   config: {
     load_config_file: false,
 
-    site_url: 'https://game-blog-dev.netlify.app',
+    site_url: SITE_URL,
 
-    backend: {
-      name: 'git-gateway',
-      branch: 'main',
+    backend,
+
+    local_backend: {
+      url: `http://${URL_HOSTNAME}:8081/api/v1`,
+      allowed_hosts: [URL_HOSTNAME],
     },
 
-    local_backend: { allowed_hosts: ['192.168.90.19', '127.0.0.1'] },
     publish_mode: 'editorial_workflow',
-    media_folder: 'public/uploaded/',
+
+    media_folder: baseMediaFolder,
+    public_folder: basePublicFolder,
+
     collections: [
       reviews,
       news,
