759a86472ea947b8b493f44b3a3a5844e2758aac
Anton <=>
2024-10-01 16:34:53 +0300

    fix(components): add whiteSpace: 'pre-wrap'

--- components/ui/Markdown/Markdown.tsx
+++ components/ui/Markdown/Markdown.tsx
@@ -1,14 +1,14 @@
+import type { FC } from 'react';
+import type { MarkdownProps } from './Markdown.types';
 import Box from '@mui/material/Box';
 import ReactMarkdown from 'react-markdown';
-import type { FC } from 'react';
 import MarkdownAnchor from './MarkdownAnchor';
 import MarkdownImg from './MarkdownImg';
-import type { MarkdownProps } from './Markdown.types';
 
 
 const Markdown: FC<MarkdownProps> = ({ children, ...props }) => {
   return (
-    <Box sx={{ p: { pb: 4 } }}>
+    <Box sx={{ p: { whiteSpace: 'pre-wrap' } }}>
       <ReactMarkdown components={{ img: MarkdownImg, a: MarkdownAnchor }} {...props}>
         {children}
       </ReactMarkdown>
